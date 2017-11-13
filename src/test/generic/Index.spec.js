d41b770f08f3f81b318e6c09dba498dc9f5d4508
pb <mail@paberr.net>
2017-11-13 15:24:21 +0100

    new index test

--- src/test/generic/Index.spec.js
+++ src/test/generic/Index.spec.js
@@ -34,4 +34,69 @@ describe('Index', () => {
             await db.destroy();
         })().then(done, done.fail);
     });
+
+    it('can handle values not conforming to index', (done) => {
+        (async function () {
+            // Write something into an object store.
+            let db = new JDB.JungleDB('indexTest', 1);
+            let st = db.createObjectStore('testStore');
+            st.createIndex('depth', ['treeInfo', 'depth']);
+
+            await db.connect();
+
+            // Fill object store.
+            const expectedJs = new Set();
+            for (let i=0; i<10; ++i) {
+                for (let j=0; j<5; ++j) {
+                    expectedJs.add(j);
+                    await st.put(`${i}-${j}`, {'val': i*10+j, 'treeInfo': {'depth': i, 'j': j}});
+                    await st.put('head', `${i*10+j}`);
+                }
+            }
+            const tx = st.transaction();
+            for (let i=10; i<50; ++i) {
+                for (let j=0; j<5; ++j) {
+                    expectedJs.add(j);
+                    await tx.put(`${i}-${j}`, {'val': i * 10 + j, 'treeInfo': {'depth': i, 'j': j}});
+                    await tx.put('head', `${i * 10 + j}`);
+                }
+            }
+            await tx.commit();
+
+            // Retrieve values at specific depth.
+            expect(await st.get('head')).toBe(`${49*10+4}`);
+
+            const atDepth5 = await st.values(JDB.Query.eq('depth', 5));
+            const atDepth15 = await st.values(JDB.Query.eq('depth', 15));
+
+            let actualJs = new Set();
+            for (const obj of atDepth5) {
+                expect(obj['treeInfo']['depth']).toBe(5);
+                actualJs.add(obj['treeInfo']['j']);
+            }
+            expect(actualJs).toEqual(expectedJs);
+
+            actualJs = new Set();
+            for (const obj of atDepth15) {
+                expect(obj['treeInfo']['depth']).toBe(15);
+                actualJs.add(obj['treeInfo']['j']);
+            }
+            expect(actualJs).toEqual(expectedJs);
+
+            // Retrieve values in depth range for given j.
+            const range = await st.values(JDB.Query.within('depth', 5, 15));
+            const expectedIs = Set.from([5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]);
+            for (let j=0; j<5; ++j) {
+                const actualIs = new Set();
+                for (const obj of range) {
+                    if (obj['treeInfo']['j'] === j) {
+                        actualIs.add(obj['treeInfo']['depth']);
+                    }
+                }
+                expect(actualIs).toEqual(expectedIs);
+            }
+
+            await db.destroy();
+        })().then(done, done.fail);
+    });
 });
