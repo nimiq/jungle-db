1da7e4cad1a81c555f189d6cd8ffa98236745ce3
styppo <mail@styp-rekowsky.de>
2018-02-16 10:13:04 -0600

    Use optimized toBase64()

--- src/test/generic/utils/BufferUtils.spec.js
+++ src/test/generic/utils/BufferUtils.spec.js
@@ -1,4 +1,5 @@
 describe('BufferUtils', () => {
+    if (typeof window === 'undefined' && typeof require === 'function') btoa = require('btoa'); //eslint-disable-line no-global-assign
 
     it('has fromBase64 and toBase64 methods', () => {
         expect(BufferUtils.toBase64(BufferUtils.fromBase64('dGVzdA=='))).toEqual('dGVzdA==');
@@ -6,4 +7,15 @@ describe('BufferUtils', () => {
         expect(BufferUtils.toBase64(BufferUtils.fromBase64('dGVzdA'))).toEqual('dGVzdA==');
     });
 
+    it('toBase64/fromBase64 handle all code points like btoa/atob', () => {
+        const arr = [];
+        for (let i = 0; i < 256; ++i) arr.push(i);
+        const sb = new Uint8Array(arr);
+        const tobase64 = BufferUtils.toBase64(sb);
+        const withbtoa = btoa(String.fromCharCode(...sb));
+
+        expect(tobase64).toEqual(withbtoa);
+        expect(BufferUtils.fromBase64(tobase64)).toEqual(sb);
+        expect(BufferUtils.fromBase64(withbtoa)).toEqual(sb);
+    });
 });
