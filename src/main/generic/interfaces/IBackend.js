f2e628c3c5ee1fc7667c22da29b34b1953ecad25
Pascal Berrang <mail@paberr.net>
2018-03-27 10:37:31 -0600

    Cleanup interfaces.

--- src/main/generic/interfaces/IBackend.js
+++ src/main/generic/interfaces/IBackend.js
@@ -1,9 +1,9 @@
 /**
  * This interface provides the methods specific to backends.
  * @interface
- * @extends {IObjectStore}
+ * @implements {IReadableObjectStore}
  */
-class IBackend extends IObjectStore{
+class IBackend extends IReadableObjectStore {
     /**
      * Returns the necessary information in order to flush a combined transaction.
      * @abstract
@@ -11,5 +11,32 @@ class IBackend extends IObjectStore{
      * @returns {Promise.<*|function():Promise>} For non-persistent backends: a function that effectively applies the transaction.
      * Native backends otherwise specify their own information as needed by their JungleDB instance.
      */
-    async applyCombined(tx) {}// eslint-disable-line no-unused-vars
+    async applyCombined(tx) {} // eslint-disable-line no-unused-vars
+
+    /**
+     * Creates a new secondary index on the object store.
+     * Currently, all secondary indices are non-unique.
+     * They are defined by a key within the object or alternatively a path through the object to a specific subkey.
+     * For example, ['a', 'b'] could be used to use 'key' as the key in the following object:
+     * { 'a': { 'b': 'key' } }
+     * Secondary indices may be multiEntry, i.e., if the keyPath resolves to an iterable object, each item within can
+     * be used to find this entry.
+     * If a new object does not possess the key path associated with that index, it is simply ignored.
+     *
+     * This function may only be called before the database is connected.
+     * Moreover, it is only executed on database version updates or on first creation.
+     * @abstract
+     * @param {string} indexName The name of the index.
+     * @param {string|Array.<string>} [keyPath] The path to the key within the object. May be an array for multiple levels.
+     * @param {{multiEntry:?boolean, unique:?boolean, upgradeCondition:?boolean|?function(oldVersion:number, newVersion:number):boolean}|boolean} [options] An options object (for deprecated usage: multiEntry boolean).
+     */
+    createIndex(indexName, keyPath, options=false) {} // eslint-disable-line no-unused-vars
+
+    /**
+     * Deletes a secondary index from the object store.
+     * @abstract
+     * @param indexName
+     * @param {{upgradeCondition:?boolean|?function(oldVersion:number, newVersion:number):boolean}} [options]
+     */
+    deleteIndex(indexName, options={}) {} // eslint-disable-line no-unused-vars
 }
